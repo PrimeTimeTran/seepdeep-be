3201f3930d65fff1f5b0d6119ec3c2a2fe14fc9e
primetimetran <primetimetran@gmail.com>
2024-04-22 00:00:26 -0400

    Submission creation working with associated user

--- server/API/submissions/index.post.js
+++ server/API/submissions/index.post.js
@@ -1,8 +1,21 @@
-export default defineEventHandler(async (event) => {
-  const body = await readBody(event);
+import { defineEventHandler, readBody } from 'h3'
+
+export default defineEventHandler(async (e) => {
   try {
-    return await new Submission(body).save();
+    const body = await readBody(e)
+    const user = e.context.user
+    if (!user) {
+      throw new Error('User not found in context')
+    }
+    const submission = new Submission({...body, user: user._id});
+    await submission.save();
+    user.submissions.push(submission._id);
+    await user.save()
+    return {
+      data: submission,
+    }
   } catch (error) {
-    return error;
+    console.error('Error while creating submission:', error)
+    return error
   }
-});
+})
