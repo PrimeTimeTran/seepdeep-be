b7cdd9e29ad1004929e0a3f8771e2fe1e13c04e0
primetimetran <primetimetran@gmail.com>
2024-04-28 17:04:54 -0400

    WIP Solvr

--- server/models/Solve.model.js
+++ server/models/Solve.model.js
@@ -4,6 +4,45 @@ import { Auditor } from './Audit/Audit';
 const solveSchema = new Schema({
   user: { type: Schema.Types.ObjectId, ref: 'User' },
   problem: { type: Schema.Types.ObjectId, ref: 'Problem' },
+  problemName: { type: String },
+  level:{
+    type: String,
+    enum: ['Encountered', 'Novice', 'Apprentice', 'Proficient', 'Intermediate', 'Advanced', 'Expert', 'Legend', 'Guru'],
+  },
+  nextSolve: {
+    type: Date
+  },
+  // Twice as long as the solve. If the user doesn't solve by penalty date we make them drop by one point
+  nextPenalty: {
+    type: Date
+  },
+  countEncountered: {
+    type: Number
+  },
+  countNovice: {
+    type: Number
+  },
+  countApprentice: {
+    type: Number
+  },
+  countProficient: {
+    type: Number
+  },
+  countIntermediate: {
+    type: Number
+  },
+  countAdvanced: {
+    type: Number
+  },
+  countExpert: {
+    type: Number
+  },
+  countLegend: {
+    type: Number
+  },
+  countGuru: {
+    type: Number
+  },
 });
 Auditor.addHooks(solveSchema);
 const Solve = mongoose.model('Solve', solveSchema);
