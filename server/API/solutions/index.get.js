e5fef7e0f573e9adec4b49e03bea1507dcd654f3
primetimetran <primetimetran@gmail.com>
2024-05-02 07:39:06 -0400

    add api for solutions

---
+++ server/API/solutions/index.get.js
@@ -0,0 +1,29 @@
+import _ from 'lodash'
+import mongoose from 'mongoose'
+
+export default defineEventHandler(async (e) => {
+  try {
+    let { limit, page } = e.context
+    let params = getQuery(e)
+    const subDocs = await Submission.find({
+      isShared: true,
+      problem: new mongoose.Types.ObjectId(params.problem),
+    })
+      .sort({ createdAt: -1 })
+      .skip((page - 1) * limit)
+      .limit(20)
+    const response = {
+      meta: {
+        page,
+        pageCount: Math.ceil(subDocs.length / limit),
+        totalCount: subDocs.length,
+      },
+      data: subDocs,
+    }
+    return response
+  } catch (error) {
+    console.log({
+      error,
+    })
+  }
+})
