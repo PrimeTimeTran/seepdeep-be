cd8c21c20b18fde80cc57f50201de88f93be3920
primetimetran <primetimetran@gmail.com>
2024-05-15 13:14:31 -0400

    Fix user

--- server/API/auth/create.post.ts
+++ server/API/auth/create.post.ts
@@ -2,36 +2,39 @@ export default defineEventHandler(async (e) => {
   try {
     let body = await readBody(e)
     body = JSON.parse(body)
-    User.findOne({ email: body.email }).then(async (u) => {
-      if (u) {
-        throw createError({
-          statusCode: 400,
-          statusMessage: 'Error: Email taken',
-        })
-      }
-
-      const hash = await encryptPassword(body.password)
-      const result = await decryptPassword(body.password, hash)
-
-      const user = await new User({
-        email: body.email,
-        passwordDigest: hash,
+    let user = await User.findOne({ email: body.email })
+    if (user) {
+      throw createError({
+        statusCode: 400,
+        statusMessage: 'Error: Email taken',
       })
+    }
 
-      await user.save()
-      const token = await jwtSign({ userId: user._id })
-      const resp = {
-        user,
-        result,
-        token,
-      }
-      logger.info({resp}, 'Userr account created.')
-      return resp
+    const hash = await encryptPassword(body.password)
+    const result = await decryptPassword(body.password, hash)
+
+    user = await new User({
+      email: body.email,
+      passwordDigest: hash,
     })
+
+    await user.save()
+    const token = await jwtSign({ userId: user._id })
+    const newUser = {
+      ...user.toJSON(),
+      id: user._id
+    }
+    const resp = {
+      user: newUser,
+      result,
+      token,
+    }
+    logger.info({ resp }, 'User account created.')
+    return resp
   } catch (error) {
-    logger.fatal({error})
+    logger.fatal({ error })
     return {
-      error
+      error,
     }
   }
 })
