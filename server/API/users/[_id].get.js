ea03a0fed8c52eef36388a21e201322c97b46e88
PrimeTimeTran <primetimetran@gmail.com>
2025-06-22 04:19:17 +0700

    Add problemTitle

--- server/API/users/[_id].get.js
+++ server/API/users/[_id].get.js
@@ -3,7 +3,13 @@ import { getRouterParam } from 'h3'
 export default defineEventHandler(async (event) => {
   try {
     const id = getRouterParam(event, '_id')
-    const user = await User.findOne({ _id: id }).populate('submissions')
+    const user = await User.findOne({ _id: id }).populate({
+      path: 'submissions',
+      populate: {
+        path: 'problem',
+        select: '_id title',
+      },
+    })
     return {
       data: user,
     }
