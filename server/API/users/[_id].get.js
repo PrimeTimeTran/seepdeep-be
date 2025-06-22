422a52c8955ff7d381d5d75a0a81e3c2fa04ace6
PrimeTimeTran <primetimetran@gmail.com>
2025-06-22 09:53:28 +0700

    Add desc order query to profile submissions

--- server/API/users/[_id].get.js
+++ server/API/users/[_id].get.js
@@ -5,6 +5,7 @@ export default defineEventHandler(async (event) => {
     const id = getRouterParam(event, '_id')
     const user = await User.findOne({ _id: id }).populate({
       path: 'submissions',
+      options: { sort: { createdAt: -1 } },
       populate: {
         path: 'problem',
         select: '_id title',
